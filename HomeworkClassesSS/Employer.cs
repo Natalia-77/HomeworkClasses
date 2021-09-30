1eb54871657b9e73b0636281f9b5e9dc9df67fba
vlasnata80@gmail.com <vlasnata80@gmail.com>
2021-09-30 22:44:32 +0300

    start task

---
+++ HomeworkClassesSS/Employer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkClassesSS
+{
+    public class Employer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Date { get; }            
+        public decimal Salary { get; set; }
+
+        public Employer(string name,string surname, DateTime date,decimal salary)
+        {
+             Name = name; ;
+            Surname=surname;
+            Date=date;
+
+
+        }
+    }
+}
---
+++ HomeworkClassesSS/Factory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkClassesSS
+{
+     public class Factory
+    {
+        public string Name { get; set; }
+        private Employer[] employeers = new Employer[5];
+        private Product[] products = new Product[20];
+       
+
+        public Employer[] Employers
+        {
+            get
+            {
+                return employeers;
+            }
+            set
+            {
+                employeers = value;
+            }
+        }
+
+        public Product[] Products
+        {
+            get
+            {
+                return products;
+            }
+            set
+            {
+                products = value;
+            }
+        }
+
+
+
+
+    }
+}
